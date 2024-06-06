0f3b7d28aa71bf206b2699223048da64883506aa
FuadMerlez <fuad.ariel@hotmail.com>
2024-06-06 14:26:57 -0400

    cambio

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Nice, you triggered this alert message!', 'success')
+  })
+}
+
 $(document).ready(function(){
    $('[data-bs-toggle="popover"]').popover({
       placement: 'top',
@@ -16,7 +36,3 @@ function launch_toast() {
    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
 }
 
-function alertButton() {
-   alert("Do you like this?");
-   console.log("Alerted");
-}
